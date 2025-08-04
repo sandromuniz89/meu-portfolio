197392aeefe972c4a66a5e77cf2922e019fa3646
Sandro Muniz <sandromuniz@gmail.com>
2025-08-04 12:35:01 -0300

    Versão inicial do meu site de portfólio

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    // Lógica para o menu hamburger em telas pequenas
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+
+    // Fecha o menu ao clicar em um link
+    navLinks.forEach(link => link.addEventListener("click", () => {
+        if (navMenu.classList.contains("active")) {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        }
+    }));
+});
